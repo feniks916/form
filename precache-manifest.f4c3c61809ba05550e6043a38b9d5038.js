a4f8f6f7438f03bbad104ad245a30e1a9dd0cf08
feniks916 <53240482+feniks916@users.noreply.github.com>
2020-05-04 15:56:27 +0300

    Updates

---
+++ precache-manifest.f4c3c61809ba05550e6043a38b9d5038.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3607faf9f407ca7c64c955acaee98f86",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "91dff9ddc42f3341db72",
+    "url": "/form/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "048efaea0d059803669b",
+    "url": "/form/static/css/main.99cb823b.chunk.css"
+  },
+  {
+    "revision": "91dff9ddc42f3341db72",
+    "url": "/form/static/js/2.6233790c.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/form/static/js/2.6233790c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "048efaea0d059803669b",
+    "url": "/form/static/js/main.929031cc.chunk.js"
+  },
+  {
+    "revision": "745d4f832f5b0f569022",
+    "url": "/form/static/js/runtime-main.091da31f.js"
+  }
+]);
